e9bd806df10af57515afb2c20fa0c04e2af926b2
fengshen <fengshenjun@yeah.net>
2024-03-20 18:01:57 +0800

    修复多次调用的问题

--- src/libs/fslog/log.cpp
+++ src/libs/fslog/log.cpp
@@ -8,15 +8,19 @@
 
 void init_log(LogLevel level, std::string file, int size, int count)
 {
-    std::vector<spdlog::sink_ptr> sinks;
-    auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    sinks.emplace_back(stdout_sink);
+    auto logger = spdlog::get("log");
+    if (!logger) {
+        std::vector<spdlog::sink_ptr> sinks;
+        auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        sinks.emplace_back(stdout_sink);
+        auto logger = std::make_shared<spdlog::logger>("log", sinks.begin(), sinks.end());
+        spdlog::register_logger(logger);
+    }
     if (!file.empty()) {
+        auto& sinks = logger->sinks();
         auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(file, size, count);
         sinks.emplace_back(rotating_sink);
     }
-    auto logger = std::make_shared<spdlog::logger>("log", sinks.begin(), sinks.end());
-    spdlog::register_logger(logger);
     spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e %t %s:%#] <%^%L%$> %v");
     spdlog::set_level((spdlog::level::level_enum)level);
     spdlog::flush_on((spdlog::level::level_enum)level);
